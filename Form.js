2f45973271945dcfb6e0ec94368818444d7baa53
mmirza55 <muskanm2122@gmail.com>
2025-04-25 21:04:15 +0100

    After School Project

---
+++ Form.js
@@ -0,0 +1,19 @@
+<DOCTYPE>
+    <html>
+        <head>
+            Form
+        </head>
+        <body>
+            function validate(e) {
+                e.preventDefault();
+                const email = document.getElementById("email").value;
+                const pass = document.getElementById("pass").value;
+                const age = document.getElementById("age").value;
+                const msgBox = document.getElementById("message");
+                let message = "";
+                if email === 
+            }
+
+        </body>
+    </html>
+</DOCTYPE>
